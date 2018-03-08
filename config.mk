5cfa9bf1416c99e5111bfb73b26d1eedc701eba9
Digital-Chaos <james.wright@jigsawdezign.com>
2018-03-08 20:05:08 +0000

    Fix FreeBSD paths

--- config.mk
+++ config.mk
@@ -5,10 +5,10 @@ VERSION = 0
 
 # paths
 PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/share/man
+MANPREFIX = ${PREFIX}/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/local/include
+X11LIB = /usr/local/lib
 
 # flags
 CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
