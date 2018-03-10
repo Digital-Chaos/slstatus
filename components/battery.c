ecf876081f5fe93e50978f66d7a7723aad149d38
Digital-Chaos <james.wright@jigsawdezign.com>
2018-03-10 02:44:56 +0000

    Fix battery component for FreeBSD

--- components/battery.c
+++ components/battery.c
@@ -1,56 +1,51 @@
 /* See LICENSE file for copyright and license details. */
-#include <limits.h>
 #include <stdio.h>
-#include <string.h>
-
+#include <sys/sysctl.h>
 #include "../util.h"
 
-const char *
-battery_perc(const char *bat)
-{
-	int perc;
-	char path[PATH_MAX];
+#define SYSCTL_BATTERY_LIFE     "hw.acpi.battery.life"
+#define SYSCTL_BATTERY_STATE    "hw.acpi.battery.state"
+#define SYSCTL_BATTERY_TIME     "hw.acpi.battery.time"
 
-	snprintf(path, sizeof(path), "%s%s%s", "/sys/class/power_supply/", bat, "/capacity");
-	return (pscanf(path, "%i", &perc) == 1) ?
-	       bprintf("%d", perc) : NULL;
+#define DISCHARGING_SYMBOL      "-"
+#define CHARGING_SYMBOL         "+"
+#define CHARGED_SYMBOL          "="
+
+static int sysctl_fetch(const char *name);
+
+const char *
+battery_perc(void)
+{
+	int battery_life = sysctl_fetch(SYSCTL_BATTERY_LIFE);
+	return (battery_life > -1) ? (bprintf("%d", battery_life)) : (NULL);
 }
 
 const char *
-battery_power(const char *bat)
+battery_power(void)
 {
-	int watts;
-	char path[PATH_MAX];
-
-	snprintf(path, sizeof(path), "%s%s%s", "/sys/class/power_supply/", bat, "/power_now");
-	return (pscanf(path, "%i", &watts) == 1) ?
-	       bprintf("%d", (watts + 500000) / 1000000) : NULL;
+	return NULL;
 }
 
 const char *
-battery_state(const char *bat)
+battery_state(void)
 {
-	struct {
-		char *state;
-		char *symbol;
-	} map[] = {
-		{ "Charging",    "+" },
-		{ "Discharging", "-" },
-		{ "Full",        "=" },
-		{ "Unknown",     "/" },
-	};
-	size_t i;
-	char path[PATH_MAX], state[12];
+	int battery_state = sysctl_fetch(SYSCTL_BATTERY_STATE);
+	return  (battery_state == 1) ? (DISCHARGING_SYMBOL) :
+	       ((battery_state == 2) ? (CHARGING_SYMBOL) :
+	       (CHARGED_SYMBOL));
+}
 
-	snprintf(path, sizeof(path), "%s%s%s", "/sys/class/power_supply/", bat, "/status");
-	if (pscanf(path, "%12s", state) != 1) {
-		return NULL;
-	}
+const char *
+battery_time(void)
+{
+	int battery_time = sysctl_fetch(SYSCTL_BATTERY_TIME);
+	return (battery_time > -1) ? (bprintf("%dh:%dm", battery_time / 60, battery_time % 60)) : (NULL);
+}
 
-	for (i = 0; i < LEN(map); i++) {
-		if (!strcmp(map[i].state, state)) {
-			break;
-		}
-	}
-	return (i == LEN(map)) ? "?" : map[i].symbol;
+static int
+sysctl_fetch(const char *name)
+{
+	int value;
+	size_t value_size = sizeof(value);
+	return (sysctlbyname(name, &value, &value_size, NULL, 0) == 0) ? (value) : (-1);
 }
